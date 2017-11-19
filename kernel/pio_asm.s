7d7a91d6707dcde480cc28ff68f2cfebfa4d52f2
Lovino Maxime <maximelovino@gmail.com>
2017-11-19 11:26:27 +0100

    Removed useless enter and leave from pio_asm, closes #29

--- kernel/pio_asm.s
+++ kernel/pio_asm.s
@@ -3,35 +3,27 @@ global outw;
 global inb;
 global inw;
 
-;TODO remove enter and leave and put ebp+4 and ebp+8 instead of ebp+8 and ebp+12
 outb:
-	enter 0,0
-	mov dx,word[ebp+8]; Here we have the port
-	mov al,byte[ebp+12]; Here is the data
+	mov dx,word[esp+4]; Here we have the port
+	mov al,byte[esp+8]; Here is the data
 	out dx,al;
-	leave;
 	ret;
 
 outw:
-	enter 0,0
-	mov dx,word[ebp+8]; Here we have the port
-	mov ax,word[ebp+12]; Here is the data
+	mov dx,word[esp+4]; Here we have the port
+	mov ax,word[esp+8]; Here is the data
 	out dx,ax;
-	leave;
 	ret;
 
 inb:
-	enter 0,0;
 	mov eax,0;
-	mov dx,word[ebp+8]; The port
+	mov dx,word[esp+4]; The port
 	in al,dx;
 	leave;
 	ret;
 
 inw:
-	enter 0,0;
 	mov eax,0;
-	mov dx,word[ebp+8]; The port
+	mov dx,word[esp+4]; The port
 	in ax,dx;
-	leave;
 	ret;
